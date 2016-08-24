9166cae2977a3bc251c9920bebc447a3c92df60f
eduardo-silva <eduardo.lour.silva@gmail.com>
2016-08-24 17:33:09 -0300

    fixed

--- Trinity.Wcf.Log4Net/Log4NetErrorHandler.cs
+++ Trinity.Wcf.Log4Net/Log4NetErrorHandler.cs
@@ -14,10 +14,9 @@ namespace Trinity.Wcf.Log4Net
     //http://pieterderycke.wordpress.com/2012/09/05/logging-all-unhandled-exception-in-wcf-with-log4net/
     public class Log4NetErrorHandler : IErrorHandler
     {
-        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-
         public bool HandleError(Exception error)
         {
+            var log = LogManager.GetLogger(error.TargetSite.ReflectedType);
             log.Error(error);
             return false; // Exception has to pass the stack further
         }
